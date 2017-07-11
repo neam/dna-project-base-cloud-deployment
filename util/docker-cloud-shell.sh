e4f10f0502b871612a6af4059b462635c08c14fb
Fredrik Wollsén <fredrik@neam.se>
2017-07-11 12:54:07 +0300

    Restored docker-cloud-shell util script

--- util/docker-cloud-shell.sh
+++ util/docker-cloud-shell.sh
@@ -4,86 +4,45 @@
 # fail on any error
 set -o errexit
 
-# Usage: deploy.sh <stackname>
+# debug
+#set -x
 
-if [ "$DEPLOYMENTS_ROOT" == "" ]; then
-    DEPLOYMENTS_ROOT=deployments
-fi
+export DATA=%DATA%
+export COMMITSHA=""
+export BRANCH_TO_DEPLOY=""
+source deploy/prepare.sh
 
-if [ "$1" == "" ]; then
+# Usage: deploy.sh <stack-deployment-directory>
 
-  function servicename {
+DEPLOYMENT_DIR="$1"
+STACK_NAME="$(basename $DEPLOYMENT_DIR)"
+STACK_UUID=$(cat $DEPLOYMENT_DIR/.docker-cloud-stack-id)
 
-      local STR=$1
+echo "Starting a remote shell in docker-cloud for a specific stack (accessing Docker Cloud via user 'DOCKERCLOUD_USER=$DOCKERCLOUD_USER')"
 
-      # Permitted characters: [0-9,a-z,A-Z] (basic Latin letters, digits 0-9)
-      STR=${STR//\//}
-      STR=${STR//./}
-      STR=${STR//-/}
-      STR=${STR//_/}
-      STR="$(echo $STR | tr '[:upper:]' '[:lower:]')" # UPPERCASE to lowercase
-      # Max length 64 chars
-      STR=${STR:0:64}
-
-      echo "$STR"
-
-  }
-
-  # choose the latest stack
-  export STACK_NAME=$(ls $DEPLOYMENTS_ROOT/ | grep $(servicename "-$APPVHOST-$COMMITSHA") | tail -n 1)
-  if [ "$STACK_NAME" == "" ]; then
-    echo "No stack found at $DEPLOYMENTS_ROOT/"$(servicename "-$APPVHOST-$COMMITSHA")"/"
-    exit 1
-  fi
-
-else
-  if [ -d "$DEPLOYMENTS_ROOT/$1" ]; then
-    export STACK_NAME=$1
-  fi
-  if [ -d "$1" ]; then
-    export STACK_NAME=$(basename $1)
-  fi
-fi
-
-export DEPLOYMENT_DIR="$DEPLOYMENTS_ROOT/$STACK_NAME"
-cd "$DEPLOYMENT_DIR"
-
-STACK_ID=$(cat .docker-cloud-stack-id)
-docker-cloud stack inspect $STACK_ID > .docker-cloud-stack.json
-WORKER_CONTAINER_ID=$(cat .docker-cloud-stack.json | jq '.services | map(select(.name == "worker"))' | jq -r '.[0].containers[0]' | awk -F  "/" '{print $5}')
-# docker-cloud container inspect $WORKER_CONTAINER_ID > .docker-cloud-worker-container.json
-
-docker-cloud exec $WORKER_CONTAINER_ID
-
-exit 0
-
-echo "Init and connect:"
-echo "export SSH_PORT=$SSH_PORT"
-echo "export SSH_FQDN=$SSH_FQDN"
-echo 'scp -r -P $SSH_PORT '$DEPLOYMENT_DIR'/.env root@$SSH_FQDN:/.env'
-echo 'scp -r -P $SSH_PORT .files/'$DATA'/media/* root@$SSH_FQDN:/files/'$DATA'/media/'
 echo
-echo "Use local ssh keys:"
-echo "echo 'Host $SSH_FQDN' >> ~/.ssh/config"
-echo "echo '	ForwardAgent yes' >> ~/.ssh/config"
+echo "* Fetching current information about the stack, identified by $STACK_UUID"
 echo
-echo "Connect:"
-echo 'ssh -p $SSH_PORT root@$SSH_FQDN'
+
+docker run -it -e DOCKERCLOUD_USER=$DOCKERCLOUD_USER -e DOCKERCLOUD_PASS=$DOCKERCLOUD_PASS -v "$(pwd)/$DEPLOYMENT_DIR:/deployment-dir" dockercloud/cli service ps --stack=$STACK_UUID \
+ | tee "$DEPLOYMENT_DIR/.docker-cloud-stack-service-ps.out"
+
+WORKER_SERVICE_ID=$(cat "$DEPLOYMENT_DIR/.docker-cloud-stack-service-ps.out" | grep 'phpfiles' | awk '{ print $2 }')
+
 echo
-echo "When connected:"
-echo "source /.env"
+echo "* Fetching current information about the worker service, identified by $WORKER_SERVICE_ID"
 echo
-echo "# File permissions"
-echo "chown -R \$WEB_SERVER_POSIX_USER:\$WEB_SERVER_POSIX_GROUP /files"
+
+docker run -it -e DOCKERCLOUD_USER=$DOCKERCLOUD_USER -e DOCKERCLOUD_PASS=$DOCKERCLOUD_PASS -v "$(pwd)/$DEPLOYMENT_DIR:/deployment-dir" dockercloud/cli container ps --service=$WORKER_SERVICE_ID \
+ | tee "$DEPLOYMENT_DIR/.docker-cloud-stack-worker-service-container-ps.out"
+
+WORKER_CONTAINER_ID=$(cat "$DEPLOYMENT_DIR/.docker-cloud-stack-worker-service-container-ps.out" | grep 'phpfiles-1' | awk '{ print $2 }')
+
 echo
-echo "# Be able to run commands like reset-db etc"
-echo "git clone --recursive \$PROJECT_GIT_REPO /app"
-echo "cd /app"
-echo "cp /.env .env"
-echo "git checkout "$COMMITSHA
-echo "PREFER=dist stack/src/install-deps.sh"
-echo "PREFER=source stack/src/install-deps.sh"
+echo "* Starting a remote shell in the worker service's first container, identified by $WORKER_CONTAINER_ID"
 echo
-echo "Then run commands"
+
+docker run -it -e DOCKERCLOUD_USER=$DOCKERCLOUD_USER -e DOCKERCLOUD_PASS=$DOCKERCLOUD_PASS -v "$(pwd)/$DEPLOYMENT_DIR:/deployment-dir" dockercloud/cli container exec $WORKER_CONTAINER_ID \
+  /bin/bash
 
 exit 0
